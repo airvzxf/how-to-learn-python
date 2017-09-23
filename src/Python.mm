05d66c49541f447a69c27a4a93f575f9407d10d8
Israel Roldan <israel.alberto.rv@gmail.com>
2017-09-23 16:53:05 -0400

    Added the first characteristics

--- src/Python.mm
+++ src/Python.mm
@@ -1,4 +1,52 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1506199093389" ID="ID_1343528659" MODIFIED="1506199107039" TEXT="Python"/>
+<node CREATED="1506199093389" ID="ID_1343528659" MODIFIED="1506199865211" TEXT="Python">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199402783" ID="ID_245689048" MODIFIED="1506199865211" POSITION="right" STYLE="fork" TEXT="Characteristic">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199464642" ID="ID_640087824" MODIFIED="1506199865211" TEXT="High-level">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199488672" ID="ID_1214617911" MODIFIED="1506199865211" TEXT="General propouse">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199502473" ID="ID_1125565734" MODIFIED="1506199865211" TEXT="Programming Paradigmas">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199515730" ID="ID_823571201" MODIFIED="1506199865211" TEXT="Functional">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199518445" ID="ID_87608738" MODIFIED="1506199865211" TEXT="Structured">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199525283" ID="ID_956453263" MODIFIED="1506199865211" TEXT="Object-Oriented">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199537494" ID="ID_1732388135" MODIFIED="1506199865211" TEXT="Aspect-Oriented">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199749655" ID="ID_884331719" MODIFIED="1506199865211" TEXT="Imperative">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+<node CREATED="1506199821752" ID="ID_407443156" MODIFIED="1506199912888" TEXT="Procedural styles">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+</node>
+</node>
+<node CREATED="1506199646301" ID="ID_1462227560" MODIFIED="1506199865211" POSITION="right" TEXT="Info">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199649067" ID="ID_1744934445" MODIFIED="1506199865211" TEXT="Created by">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199653158" ID="ID_156721189" MODIFIED="1506199865211" TEXT="Guido van Rossum">
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+</node>
+<node CREATED="1506199702076" ID="ID_682480832" MODIFIED="1506199865210" TEXT="Release">
+<font NAME="FreeMono" SIZE="24"/>
+<node CREATED="1506199706870" ID="ID_1678855166" MODIFIED="1506199865210" STYLE="fork" TEXT="20 February 1991">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="FreeMono" SIZE="24"/>
+</node>
+</node>
+</node>
+</node>
 </map>
